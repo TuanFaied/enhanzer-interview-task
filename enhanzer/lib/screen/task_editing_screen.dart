acb911b52e6161cd28194141d8d91da9c0a098b1
TuanFaied <tuanfaied8@gmail.com>
2024-10-17 21:24:41 +0530

    update task editing ui page

--- enhanzer/lib/screen/task_editing_screen.dart
+++ enhanzer/lib/screen/task_editing_screen.dart
@@ -1,4 +1,3 @@
-// task_editing_screen.dart
 import 'package:enhanzer/models/task_model.dart';
 import 'package:enhanzer/service/task_viewModel.dart';
 import 'package:flutter/material.dart';
@@ -32,60 +31,142 @@ class _TaskEditingScreenState extends State<TaskEditingScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.task == null ? 'Add Task' : 'Edit Task'),
-        actions: [IconButton(onPressed: _saveTask, icon: Icon(Icons.save)),
-        widget.task == null ? const SizedBox() : IconButton( onPressed: () => _deleteTask(Provider.of<TaskViewModel>(context, listen: false).tasks.indexOf(widget.task!)), icon: Icon(Icons.delete))],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              TextFormField(
-                initialValue: _title,
-                textInputAction: TextInputAction.next,
-                decoration: const InputDecoration(hintText: 'Task Title'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a title';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _title = value!;
-                },
-              ),
-              TextFormField(
-                initialValue: _description,
-                maxLines: null,
-                keyboardType: TextInputType.multiline,
-                decoration: const InputDecoration(hintText: 'Task Description'),
-                
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a description';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _description = value!;
-                },
-              ),
-              CheckboxListTile(
-                title: Text('Completed'),
-                value: _isCompleted,
-                onChanged: (value) {
-                  setState(() {
-                    _isCompleted = value!;
-                  });
-                },
-              ),
-              
-              
-              
-            ],
+        
+        actions: [
+          IconButton(
+            onPressed: _saveTask,
+            icon: Icon(Icons.save, color: Colors.black),
           ),
-        ),
+          widget.task == null
+              ? const SizedBox()
+              : IconButton(
+                  onPressed: () => _deleteTask(
+                    Provider.of<TaskViewModel>(context, listen: false)
+                        .tasks
+                        .indexOf(widget.task!),
+                  ),
+                  icon: Icon(Icons.delete),
+                ),
+        ],
+      ),
+      body: Stack(
+        children: [
+          // Background Image
+          // Container(
+          //   decoration: BoxDecoration(
+          //     image: DecorationImage(
+          //       image: AssetImage('assets/images/parchment_background.jpg'),
+          //       fit: BoxFit.cover,
+          //     ),
+          //   ),
+          // ),
+          Positioned.fill(
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/bg.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          // Positioned.fill(
+          //   child: Opacity(
+          //     opacity: 0.1,
+          //     child: Image.asset(
+          //       'assets/images/lines_overlay.png',
+          //       fit: BoxFit.cover,
+          //     ),
+          //   ),
+          // ),
+          // Main Form
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextFormField(
+                    initialValue: _title,
+                    textInputAction: TextInputAction.next,
+                    decoration: InputDecoration(
+                      hintText: 'Task Title',
+                      hintStyle: TextStyle(
+                        fontFamily: 'DancingScript', // Handwritten font
+                        fontSize: 22,
+                        color: Colors.brown.shade700,
+                      ),
+                      border: InputBorder.none,
+                    ),
+                    style: TextStyle(
+                      fontFamily: 'DancingScript', // Handwritten font
+                      fontSize: 22,
+                      color: Colors.black,
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a title';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _title = value!;
+                    },
+                  ),
+                  SizedBox(height: 16.0),
+                  TextFormField(
+                    initialValue: _description,
+                    maxLines: null,
+                    keyboardType: TextInputType.multiline,
+                    decoration: InputDecoration(
+                      hintText: 'Task Description',
+                      hintStyle: TextStyle(
+                        fontFamily: 'DancingScript', // Handwritten font
+                        fontSize: 18,
+                        color: Colors.brown.shade700,
+                      ),
+                      border: InputBorder.none,
+                    ),
+                    style: TextStyle(
+                      fontFamily: 'DancingScript', // Handwritten font
+                      fontSize: 18,
+                      color: Colors.black,
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a description';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _description = value!;
+                    },
+                  ),
+                  SizedBox(height: 16.0),
+                  CheckboxListTile(
+                    title: Text(
+                      'Completed',
+                      style: TextStyle(
+                        fontFamily: 'DancingScript', // Handwritten font
+                        fontSize: 20,
+                        color: Colors.brown.shade800,
+                      ),
+                    ),
+                    value: _isCompleted,
+                    onChanged: (value) {
+                      setState(() {
+                        _isCompleted = value!;
+                      });
+                    },
+                    activeColor: Colors.brown,
+                    checkColor: Colors.white,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -96,9 +177,10 @@ class _TaskEditingScreenState extends State<TaskEditingScreen> {
       final viewModel = Provider.of<TaskViewModel>(context, listen: false);
       if (widget.task == null) {
         viewModel.addTask(Task(
-          title: _title, 
+          title: _title,
           isCompleted: _isCompleted,
-          description: _description,));
+          description: _description,
+        ));
       } else {
         viewModel.updateTask(Task(
           id: widget.task!.id,
